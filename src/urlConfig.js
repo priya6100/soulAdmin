4ca74d6b8885dc2e974d3e70267647ca2c3a13d9
priya6100 <priyaranjan@grapsnextsocial.com>
2021-11-04 11:29:39 +0530

    initall com

---
+++ src/urlConfig.js
@@ -0,0 +1,8 @@
+const baseUrl =  "https://soulbackend.herokuapp.com";
+
+
+export const api = `${baseUrl}/api`;
+export const generatePublicUrl = (fileName) => {
+
+    return `${baseUrl}/public/${fileName}`;
+};
